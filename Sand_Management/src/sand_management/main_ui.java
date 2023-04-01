c2a5fe2ff76d1726da35bf7e298df253e2fcca8d
Soravith Puvekit <90691274+PinkMontyPie@users.noreply.github.com>
2023-04-02 04:16:50 +0700

    Test commit

--- Sand_Management/src/sand_management/main_ui.java
+++ Sand_Management/src/sand_management/main_ui.java
@@ -32,11 +32,11 @@ public class main_ui extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1920, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 1080, Short.MAX_VALUE)
         );
 
         pack();
