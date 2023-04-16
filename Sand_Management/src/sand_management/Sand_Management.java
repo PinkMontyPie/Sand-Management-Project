393cf3e65be0c153122eba6a922f50fbcc89982e
Soravith Puvekit <90691274+PinkMontyPie@users.noreply.github.com>
2023-04-16 21:37:41 +0700

    change FeelAndLook

--- Sand_Management/src/sand_management/Sand_Management.java
+++ Sand_Management/src/sand_management/Sand_Management.java
@@ -1,19 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package sand_management;
 
-/**
- *
- * @author mee
- */
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
 public class Sand_Management {
-    
-    public static void main(String[] args) {
-        login_ui show = new login_ui();
-        show.setVisible(true);
-        
+
+    public static void main(String[] args) throws Exception{
+        String str = "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
+        UIManager.setLookAndFeel(str);
+        JFrame loginUI = new login_ui();
+        loginUI.setVisible(true);
     }
-    
 }
